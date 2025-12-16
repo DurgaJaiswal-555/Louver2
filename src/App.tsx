d054ebc348c4404075d6ac44053e282942abd9aa
Durga <durga.jaiswal@miraiedge.com>
2025-12-16 11:46:31 +0530

    Add index.ts file for default exports

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Facilities from "@components/Facilities";
 import SportsCategory from "@components/SportsCategory";
 import ContactForm from "@components/ContactUs";
 import Testimonials from "@components/Testimonial";
-import Subscribe from "@components/Subscribe/Subscribe.component";
+import Subscribe from "@components/Subscribe";
 
 function App() {
 
