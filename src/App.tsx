a5caabdfbe41130e7089331e64e9bc325ad6f88d
Durga <durga.jaiswal@miraiedge.com>
2025-12-16 11:53:32 +0530

    Rename the ContactUS - component file and add the index.ts file

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,17 @@ import Hero from "@components/Hero/Hero";
 import About from "@components/About";
 import Facilities from "@components/Facilities";
 import SportsCategory from "@components/SportsCategory";
-import ContactForm from "@components/ContactUs/ContactUs.compononet";
+import ContactForm from "@components/ContactUs";
 
 function App() {
 
   return (
     <>
-      <Hero/>
-      <About/>
-      <Facilities/>
-      <SportsCategory/>
-      <ContactForm/>
+      <Hero />
+      <About />
+      <Facilities />
+      <SportsCategory />
+      <ContactForm />
     </>
   )
 }
