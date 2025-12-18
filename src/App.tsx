d19667a1f87f24d4eca0c18398495d867f7ec911
Durga <durga.jaiswal@miraiedge.com>
2025-12-18 15:28:06 +0530

    resolve the path issue and refactor the Hero - Section component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Hero from "@components/Hero/Hero";
+import Hero from "@components/Hero";
 import About from "@components/About";
 import Facilities from "@components/Facilities";
 import SportsCategory from "@components/SportsCategory";
